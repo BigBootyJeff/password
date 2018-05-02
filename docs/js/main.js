428d2075e28c75311806be5988f26b4f1a00bb14
Kelsey Huse <kelseyhuse30@gmail.com>
2018-05-02 11:06:34 -0400

    rename public to docs for github pages

---
+++ docs/js/main.js
@@ -0,0 +1,8 @@
+var password = 'corgibutt';
+var userResponse = '';
+
+while (userResponse !== password) {
+    var userResponse = prompt("What's the password?");
+}
+
+window.location.href = '/corgi.html';
